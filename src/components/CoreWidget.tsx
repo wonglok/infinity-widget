828061fc409ace6820522828dec174f49b23b1fe
Lok Lok <yellowhappy831@gmail.com>
2026-07-26 00:38:07 +0800

    widget

--- src/components/CoreWidget.tsx
+++ src/components/CoreWidget.tsx
@@ -11,7 +11,7 @@ export function CoreWidget({ user, app }: { user: any; app: any }) {
   return (
     <>
       <div className="p-3 bg-gray-200 h-full  bg-linear-120 from-[#dafdd1] to-[#ffb300]">
-        My Core Widget
+        My Core Widget id: {app.id}
         <div>
           <pre>User: {JSON.stringify(user)}</pre>
           <pre>App: {JSON.stringify(app)}</pre>
