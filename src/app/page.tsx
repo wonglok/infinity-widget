2b618b0e67cf5e98ea9ca15fcf3b5ac2a94ed2c5
Lok Lok <yellowhappy831@gmail.com>
2026-09-02 15:53:32 +0800

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,19 @@ import { CoreWidget } from "@/components/CoreWidget";
 export default function Home() {
   //
 
-  let mockUser = {
-    id: "mock_userID_u123456",
-    username: "mock_wonglok831",
-    email: "mock_lok@lok.com",
-  };
+  // let mockUser = {
+  //   id: "mock_userID_u123456",
+  //   username: "mock_wonglok831",
+  //   email: "mock_lok@lok.com",
+  // };
 
-  //
+  // //
 
-  let mockApp = {
-    id: "mock_ppap-app-id",
-    name: "mock_loklok cool widget",
-    origin: `https://infinity-widget.vercel.app`,
-  };
-
-  //
+  // let mockApp = {
+  //   id: "mock_ppap-app-id",
+  //   name: "mock_loklok cool widget",
+  //   origin: `https://infinity-widget.vercel.app`,
+  // };
 
   return (
     <div className="w-full h-full">
